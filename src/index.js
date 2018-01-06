557fbe50dac6be07d7430174cd6ae4f93027aa9e
Darwin Garino Manalo <darwinmanalophi@gmail.com>
2018-01-06 21:44:48 +0800

    Adding Update and Delete Actions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import store from './store'
-import { addToCart } from './actions/cart-actions'
+import { addToCart, updateCart, deleteFromCart } from './actions/cart-actions'
 
 let unsubscribe = store.subscribe(() => {
 	console.log(store.getState())
 })
 
 store.dispatch(addToCart('Cheese', 2, 10))
-store.dispatch(addToCart('Mayonnaise', 1, 15))
-store.dispatch(addToCart('Butter', 3, 20))
+store.dispatch(updateCart('Cheese', 3, 30))
+store.dispatch(deleteFromCart('Cheese'))
 
 unsubscribe()
--- src/reducers/cart-reducer.js
+++ src/reducers/cart-reducer.js
@@ -1,4 +1,4 @@
-import { ADD_TO_CART } from '../actions/cart-actions'
+import { ADD_TO_CART, UPDATE_CART, DELETE_FROM_CART } from '../actions/cart-actions'
 
 const initialState = {
 	cart: [
@@ -23,6 +23,21 @@ export default function(state = initialState, action) {
 				cart: [...state.cart, action.payload]
 			}
 		}
+
+		case UPDATE_CART: {
+			return {
+				...state,
+				cart: state.cart.map(item => item.product === action.payload.product ? action.payload: item)
+			}
+		}
+
+		case DELETE_FROM_CART: {
+			return {
+				...state,
+				cart: state.cart.filter(item => item.product !== action.payload.product)
+			}
+		}
+
 		default: 
 			return state
 	}
