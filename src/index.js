ed7994e721227d8c28665c1bb34ff4d694248890
Darwin Garino Manalo <darwinmanalophi@gmail.com>
2018-01-08 21:20:51 +0800

    Integration with React

--- src/index.js
+++ src/index.js
@@ -1,6 +1,19 @@
 import store from './store'
 import { addToCart, updateCart, deleteFromCart } from './actions/cart-actions'
 
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+
+const App = <h1>Redux Shopping Cart</h1>
+
+ReactDOM.render(
+	<Provider store={store}>
+		{ App }
+	</Provider>,
+	document.getElementById('root')
+)
+
 let unsubscribe = store.subscribe(() => {
 	console.log(store.getState())
 })
--- src/store.js
+++ src/store.js
@@ -2,6 +2,6 @@ import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers'
 
-let store = createStore(rootReducer, composeWithDevTools())
+const store = createStore(rootReducer, composeWithDevTools())
 
 export default store
